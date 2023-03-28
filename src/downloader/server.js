cd9987570d600922c8a5af924fbbd7e38f8684bc
Soumodipta Bose <sbose2019@gmail.com>
2023-03-28 22:18:57 +0530

    restructured code

--- src/downloader/server.js
+++ src/downloader/server.js
@@ -1,20 +1,43 @@
-const express = require('express');
+const express = require('express'),
+ejslayouts = require('express-ejs-layouts');
+const path = require("path");
 const app = express();
 const port = 3000;
-const dfs_server_ip = 'localhost'
-const dfs_server_port = 3001
+// const dfs_server_ip = 'localhost'
+// const dfs_server_port = 3001
+const filesendRouter = require('./routes/fileSender');
+const bodyParser = require('body-parser')
+// setup for express
+app.use(ejslayouts);
+app.set("layout", "layouts/index");
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/sendfile', filesendRouter);
+app.use("/css",express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
+app.use("/js",express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")))
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
 
-app.set("view engine", "pug");
+// routes and apis
 
 app.get('/', (req, res) => {
   res.redirect('/login');
 })
 
 app.get('/login', (req, res) => {
-  res.render('login',{title: 'Downloader'});
+  res.render('login',{title: 'Downloader Login'});
 })
-
-const filesendRouter = require('./routes/fileSender');
-app.use('/sendfile', filesendRouter);
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if the username and password are correct
+  // In a real app, this should be done using a database or other authentication system
+  if (username === 'admin' && password === 'password') {
+    res.send('Logged in successfully!');
+  } else {
+    res.send('Incorrect username or password');
+  }
+});
 
 app.listen(port);
