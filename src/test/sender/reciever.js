012b51c18210781050a9c4c606e56d4032e4f45a
Soumodipta Bose <sbose2019@gmail.com>
2023-04-14 02:40:52 +0530

    multithread working

--- src/test/sender/reciever.js
+++ src/test/sender/reciever.js
@@ -17,66 +17,49 @@ socket.on('init', ({ parts,filesize }) => {
   console.log(`Number of parts: ${parts}`);
   total_parts=parts;
   total_size=filesize;
-  // fs.writeFileSync(downloadname, Buffer.alloc(total_size));
+  fs.writeFileSync(downloadname, Buffer.alloc(total_size));
   console.log('Empty File created.. of size '+total_size);
-  fun(2,downloadname,filename,total_size);
-  socket.disconnect();
-  // for (let i = 0; i < total_parts; i++) {
-  //   pool.exec('recieveChunkedData', [i,downloadname,filename]).then(() => {
-  //     console.log('Part ' + i + ' recieved');
-  //   }).catch((err) => {
-  //     console.error(err);
-  //   });
-  // }
-  
-});
-function writeChunkToFile(filepath,chunks, part_start) {
-  const chunkData = Buffer.concat(chunks);
-  fs.open(filepath, "r+", (err, fd) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // console.log(fd);
-      fs.write(fd, chunkData,0,chunkData.length,part_start, (err, bytes) => {
-        if (err) {
-          console.log(err.message);
-        } else {
-          console.log(bytes + " bytes written");
-        }
-      });
-    }
-  });
-}
-
-function fun(partIndex,filepath,origin_path,filesize){
-
-  const socket = require('socket.io-client')('http://localhost:4001');
-    socket.on('connect', () => {
-      socket.emit('request_part', { partIndex: partIndex,filepath:origin_path });
-      // socket.on('part_handshake', () => {
-      //   socket.emit('start');
-      // });
-      const part_start = partIndex * PART_SIZE;
-      const part_end = Math.min(part_start + PART_SIZE, filesize);
-      const numChunks = Math.ceil((part_end - part_start) / CHUNK_SIZE);
-      let receivedChunks = 0;
-      const corrupted_chunks=[];
-      let chunks = [];
-      socket.on('${partIndex}-chunk', ({ index, data }) => {
-          console.log(`Received chunk ${index} of part ${partIndex}`);
-          chunks[index] = data;
-          receivedChunks++;
-          if (receivedChunks === numChunks) {
-              socket.emit('{partIndex}-end');
-          }
-        });
-      socket.on('{partIndex}-end', () => {
-          console.log('All chunks received for part '+partIndex);
-          writeChunkToFile(filepath, chunks, part_start)
-          socket.disconnect();
-      });
+  // fun(2,downloadname,filename,total_size);
+  for (let i = 0; i < total_parts; i++) {
+    pool.exec('recieveChunkedData', [i,downloadname,filename,total_size]).then(() => {
+      console.log('Part ' + i + ' recieved');
+    }).catch((err) => {
+      console.error(err);
     });
-}
+  }
+
+});
+
+
+// function fun(partIndex,filepath,origin_path,filesize){
+
+//   const socket = require('socket.io-client')('http://localhost:4001');
+//     socket.on('connect', () => {
+//       socket.emit('request_part', { partIndex: partIndex,filepath:origin_path });
+//       // socket.on('part_handshake', () => {
+//       //   socket.emit('start');
+//       // });
+//       const part_start = partIndex * PART_SIZE;
+//       const part_end = Math.min(part_start + PART_SIZE, filesize);
+//       const numChunks = Math.ceil((part_end - part_start) / CHUNK_SIZE);
+//       let receivedChunks = 0;
+//       const corrupted_chunks=[];
+//       let chunks = [];
+//       socket.on('${partIndex}-chunk', ({ index, data }) => {
+//           console.log(`Received chunk ${index} of part ${partIndex}`);
+//           chunks[index] = data;
+//           receivedChunks++;
+//           if (receivedChunks === numChunks) {
+//               socket.emit('{partIndex}-end');
+//           }
+//         });
+//       socket.on('{partIndex}-end', () => {
+//           console.log('All chunks received for part '+partIndex);
+//           writeChunkToFile(filepath, chunks, part_start)
+//           socket.disconnect();
+//       });
+//     });
+// }
 
 
 // socket.disconnect();
--- src/test/sender/recieveworker.js
+++ src/test/sender/recieveworker.js
@@ -1,36 +1,52 @@
 const workerpool = require('workerpool');
 const fs = require('fs');
-async function recieveChunkedData(partIndex,filepath,origin_path) {
-    const socket = require('socket.io-client')('http://localhost:4001');
-    socket.on('connect', () => {
-      socket.emit('request_part', { partIndex: partIndex,filepath:origin_path });
-      // socket.on('part_handshake', () => {
-      //   socket.emit('start');
-      // });
-      
-      let receivedChunks = 0;
-      const corrupted_chunks=[];
-      const chunks = [];
-      socket.on('${partIndex}-chunk', ({ index, data }) => {
-          console.log(`Received chunk ${index} of part ${partIndex}`);
-          chunks[index] = data;
-          receivedChunks++;
-          if (total_chunks === chunks.length) {
-            socket.emit('{partIndex}-end');
+const CHUNK_SIZE = 1024 * 1024; // 1MB
+const PART_SIZE = 256 * CHUNK_SIZE;
+function recieveChunkedData(partIndex,filepath,origin_path,filesize) {
+  const socket = require('socket.io-client')('http://localhost:4001');
+  socket.connect();
+  socket.emit('request_part', { partIndex: partIndex,filepath:origin_path });
+    // socket.on('part_handshake', () => {
+    //   socket.emit('start');
+    // });
+  const part_start = partIndex * PART_SIZE;
+  const part_end = Math.min(part_start + PART_SIZE, filesize);
+  const numChunks = Math.ceil((part_end - part_start) / CHUNK_SIZE);
+  let receivedChunks = 0;
+  const corrupted_chunks=[];
+  let chunks = [];
+  socket.on('${partIndex}-chunk', ({ index, data }) => {
+      console.log(`Received chunk ${index} of part ${partIndex}`);
+      chunks[index] = data;
+      receivedChunks++;
+      if (receivedChunks === numChunks) {
+          socket.emit('{partIndex}-end');
+      }
+    });
+  socket.on('{partIndex}-end', () => {
+      console.log('All chunks received for part '+partIndex);
+      writeChunkToFile(filepath, chunks, part_start)
+      socket.disconnect();
+  });
+}
+
+
+  function writeChunkToFile(filepath,chunks, part_start) {
+    const chunkData = Buffer.concat(chunks);
+    fs.open(filepath, "r+", (err, fd) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log(fd);
+        fs.write(fd, chunkData,0,chunkData.length,part_start, (err, bytes) => {
+          if (err) {
+            console.log(err.message);
+          } else {
+            console.log(bytes + " bytes written");
           }
         });
-      socket.on('{partIndex}-end', () => {
-          console.log('All chunks received for part '+partIndex);
-          writeChunkToFile(filepath, chunks, partIndex)
-          socket.disconnect();
-      });
+      }
     });
-  }
-
-
-  function writeChunkToFile(filepath,chunks, partIndex) {
-    const chunkData = Buffer.concat(chunks);
-    fs.writeFileSync(filepath, chunkData);
   }
 
   workerpool.worker({
